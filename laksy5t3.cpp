7b144f9148848bc5e0d1e4a03d8aa27afe3fa568
Lennynator <111982871+Lennynator@users.noreply.github.com>
2023-11-22 01:31:06 +0200

    Update laksy5t3.cpp
    Muutama typo

--- laksy5t3.cpp
+++ laksy5t3.cpp
@@ -5,19 +5,16 @@ private:
     int* dynaaminenTieto;
 
 public:
-    // Konstruktori, joka luo dynaamisen muuttujan
     DynaaminenLuokka() {
         dynaaminenTieto = new int(42);
         std::cout << "Luokan konstruktori, arvo: " << *dynaaminenTieto << std::endl;
     }
 
-    // Kopiokonstruktori
     DynaaminenLuokka(const DynaaminenLuokka& toinen) {
         dynaaminenTieto = new int(*(toinen.dynaaminenTieto));
         std::cout << "Kopiokonstruktori, arvo: " << *dynaaminenTieto << std::endl;
     }
 
-    // Sijoitusoperaattori
     DynaaminenLuokka& operator=(const DynaaminenLuokka& toinen) {
         if (this != &toinen) {  // Itsesijoitus tarkistus
             delete dynaaminenTieto;  // Vapautetaan vanha dynaaminen muuttuja
@@ -27,7 +24,6 @@ public:
         return *this;
     }
 
-    // Destruktori, joka vapauttaa dynaamisen muuttujan
     ~DynaaminenLuokka() {
         std::cout << "Luokan destruktori, arvo: " << *dynaaminenTieto << std::endl;
         delete dynaaminenTieto;
@@ -42,10 +38,8 @@ int main() {
     DynaaminenLuokka eka;
     DynaaminenLuokka toka;
 
-    // Sijoitus ilman kopiokonstruktoria
     toka = eka;
 
-    // Kopiokonstruktori
     DynaaminenLuokka kolmas(eka);
 
     return 0;
